125c5a7cb9f64d4232342a99d484d87b95ff3d36
Aanand Kumar <aanandmrh222@gmail.com>
2024-08-02 22:58:06 +0530

    DSA

---
+++ AK_05_Backtracking/AK_02_FIndSubsets.java
@@ -0,0 +1,26 @@
+package AK_05_Backtracking;
+
+public class AK_02_FIndSubsets {
+    public static void findSubsets(String str, String ans, int i) {
+        // base case
+        if(i == str.length()) {
+            if(ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        // recursion
+        // yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        // No choice
+        findSubsets(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+         String str = "abc";
+         findSubsets(str, "", 0);
+    }
+}
