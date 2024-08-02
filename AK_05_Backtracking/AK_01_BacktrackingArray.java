21782f67dba4a2e6c15897b13830992ea12f4624
Aanand Kumar <aanandmrh222@gmail.com>
2024-08-02 22:19:21 +0530

    DSA

--- AK_05_Backtracking/AK_01_BacktrackingArray.java
+++ AK_05_Backtracking/AK_01_BacktrackingArray.java
@@ -1,7 +1,24 @@
 package AK_05_Backtracking;
+import AK_01_ArraySorting.AK_00_PrintArray;
 
 public class AK_01_BacktrackingArray {
-    public static void main(String[] args) {
+    // Time complexity - O(n)
+    public static void changeArr(int[] arr, int i, int value) {
+        // base case
+        if(i == arr.length) {
+            AK_00_PrintArray.printArray(arr);
+            return;
+        }
+        // recursion
+        arr[i] = value;
+        changeArr(arr, i+1, value+1);  // fn call step
 
+        arr[i] = arr[i] - 2;  // backtracking step
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        changeArr(arr, 0, 1);
+        AK_00_PrintArray.printArray(arr);
     }
 }
